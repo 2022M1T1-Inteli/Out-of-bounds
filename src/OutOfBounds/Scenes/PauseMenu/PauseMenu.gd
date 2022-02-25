64d9e273cc06b2a77933c10dc7b188a74651b721
Gabriel Carneiro <gabriel.carneiro@sou.inteli.edu.br>
2022-02-25 14:53:29 -0300

    Criamos o menu de pausa do jogo
    Co-Authored-By: Antonio Ribeiro <99191811+021Antonio@users.noreply.github.com>

---
+++ src/OutOfBounds/Scenes/PauseMenu/PauseMenu.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+func _ready(): 
+	showPauseMenu(false) 
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): 
+		showPauseMenu(!get_tree().paused) 
+		get_tree().paused = !get_tree().paused #sistema de liga e desliga para o jogo 
+
+func showPauseMenu(is_visible): #is_visible é um parâmetro próprio do Godot
+	for node in get_children(): 
+		node.visible = is_visible
+
+
+func _on_ResumeBtn_pressed():
+	showPauseMenu(false)
+	get_tree().paused = false 
+
+
+func _on_QuitBtn_pressed():
+	get_tree().change_scene("res://Scenes/TelaInicial/TelaInicial.tscn")
+	showPauseMenu(false)
+	get_tree().paused = false 
