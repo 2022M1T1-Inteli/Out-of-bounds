8060ae05304eb5442d8d82a61a7306815762bdbd
Pauleradixz <paulo.evangelista@sou.inteli.edu.br>
2022-03-03 01:33:07 -0300

    Terminei ataque do personagem, adicionei layers e masks
    configurei as layers e masks do jogo, ver em (Project settings>layers>2d physics)
    personagem agora ataca e destrói a grama

---
+++ src/OutOfBounds/Scenes/Grass/grass.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+func create_grass_effect():
+	var grassEffect = load("res://Scenes/Grass/grassEffect.tscn") 
+	var GrassEffect = grassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(GrassEffect)
+	GrassEffect.global_position = global_position
+	#explicação do código: 23min do https://www.youtube.com/watch?v=1mI04gPhd3E&list=PL9FzW-m48fn2SlrW0KoLT4n5egNdX-W9a&index=10
+	#instanciando a cena GrassEffect pra dentro de world apenas quando esse código roda
+
+
+func _on_hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
