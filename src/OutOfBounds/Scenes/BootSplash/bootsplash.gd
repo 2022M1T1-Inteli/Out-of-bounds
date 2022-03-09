b66e9fe7f4e2597f31a8b61a029333376055b4af
Pauleradixz <paulo.evangelista@sou.inteli.edu.br>
2022-03-09 09:16:03 -0300

    Revert "Renomeei pastas fora do padrão"
    This reverts commit c0017c3a289d5d142b90f98dd61518fb24425374.

--- src/OutOfBounds/Scenes/BootSplash/bootsplash.gd
+++ src/OutOfBounds/Scenes/BootSplash/bootsplash.gd
@@ -1,6 +1,6 @@
 extends TextureRect
 
-var next_level = preload("res://Scenes/HomeScreen/homeScreen.tscn")
+var next_level = preload("res://Scenes/HomeScreen/HomeScreen.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
--- src/OutOfBounds/Scenes/HomeScreen/HomeScreen.gd
+++ src/OutOfBounds/Scenes/HomeScreen/HomeScreen.gd
@@ -17,10 +17,10 @@ func _ready():
 
 
 func _on_StartBtn_pressed() -> void:
-	get_tree().change_scene("res://Scenes/World/world.tscn")
+	get_tree().change_scene("res://Scenes/World/World.tscn")
 	
 func _on_Credito_pressed() -> void:
-	get_tree().change_scene("res://Scenes/About/about.tscn")
+	get_tree().change_scene("res://Scenes/About/About.tscn")
 	
 func _on_QuitBtn_pressed():
 	get_tree().quit()
