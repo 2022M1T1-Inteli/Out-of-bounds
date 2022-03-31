56a88ba2eb70af988d7cd09735a9c39bf702e006
melyssa08 <melyssa.rojas@sou.inteli.edu.br>
2022-03-31 00:00:04 -0300

    Comentários no Puzzle da Faxina

--- src/OutOfBounds/Oil.gd
+++ src/OutOfBounds/Oil.gd
@@ -1,11 +1,14 @@
 extends NinePatchRect
 
+# Variável que aciona as condições das funções desse script
 var isNear = false
+# Array que acumula os pontos.
 var arrayScore = []
 
 func _on_Area2D_Oil_body_entered(body):
 	isNear = true
 	if isNear == true:
+# Condição que apaga um elemento Oil correspondente e adiciona pontos pelo tamanho da array
 		if Input.is_action_pressed("interact"):
 			queue_free()
 			arrayScore.append(1)
